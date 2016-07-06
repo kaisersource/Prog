fd3853dc2e11985242bb68b8736f71ffda0deaae
kaisersource <emanuelebosimini@gmail.com>
2016-07-06 15:10:02 +0200

    Update EsLista0.java

--- EsLista0/src/eslista0/EsLista0.java
+++ EsLista0/src/eslista0/EsLista0.java
@@ -18,10 +18,10 @@ public class EsLista0 {
 
   
     public static void main(String[] args) {
-        Lista l = new Lista(5);
-        l.visualizza();
-        l.visualizzan(7);
-        Lista l2= new Lista(100);
+        Lista l = new Lista(5);//crea una lista di 5 elementi casuali
+        l.visualizza();//funzione per printare
+        l.visualizzan(7);//funzione per printare tutti gli elementi maggiori di 7
+        Lista l2= new Lista(100);//crea una lista di 100 elementi casuali
         l2.visualizza();
         l2.visualizzan(10);
         
