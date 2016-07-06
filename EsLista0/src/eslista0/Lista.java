33a2dac71b143a8bbb27abe7bed844dd5937b50d
kaisersource <emanuelebosimini@gmail.com>
2016-07-06 15:08:10 +0200

    Update Lista.java

--- EsLista0/src/eslista0/Lista.java
+++ EsLista0/src/eslista0/Lista.java
@@ -10,29 +10,29 @@ import java.util.Random;
  */
 public class Lista {
 
-    Elemento testa=null;
-    public Lista(int n){
+    Elemento testa=null; //creo un oggetto di tipo Elemento
+    public Lista(int n){ //gli passo la dimensione della lista
         for(int c=0;c<n;c++)
-            testa=new Elemento((int)(Math.random()*30),testa);
+            testa=new Elemento((int)(Math.random()*30),testa);//inserimento in testa di elementi con campo valore casuale
     }
     
     public void visualizza(){
         Elemento e=testa;//creo una copia di testa cosi' lavoro su 'e', quando  scorro la lista  non distruggo 'testa', ma 'e'
     
-        while(e!=null){
-            System.out.print(e.info+" ");
-            e=e.next;
+        while(e!=null){//finche' la lista non e' vuota
+            System.out.print(e.info+" ");//printa
+            e=e.next;//scorri
         }
         System.out.println("");
     
     }
     
-     public void visualizzan(int n){
+     public void visualizzan(int n){//passatogli n
     Elemento e=testa;
     
     while(e!=null){
-        if(n<e.info){
-            System.out.print(e.info+" ");
+        if(n<e.info){//se l'elemento e' maggiore di n
+            System.out.print(e.info+" ");//printa
         }
             e=e.next;
     }
